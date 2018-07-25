491c77a47839f0fde3d204c581ac8713eb70f793
Teresa Rakowsky <teresarakowsky@gmail.com>
2018-07-25 20:11:39 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -13,8 +13,20 @@
 #  - agave - 1/4 cup
 # All of the ingredients should be symbols and the amounts should be strings. Remember that symbols start with a colon and be sure to sub underscores for spaces.
 
+perfect_10_recipe = {:almond_flour => "2 cups",:gluten_free_whole_oats => 
+
+
+
+
+
+
+
 #2. Write a method that simply returns (not puts) your perfect_10 hash
+
+
 def recipe_ingredients(perfect_10_recipe)
+  
+  
 end
 
 #2: Write code that returns the amount of chocolate chips from perfect_10 ingredients that is needed in the recipe.
