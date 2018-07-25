95b1465419cdfc6979f84a97b0419475339ca191
Teresa Rakowsky <teresarakowsky@gmail.com>
2018-07-25 20:13:22 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -13,7 +13,14 @@
 #  - agave - 1/4 cup
 # All of the ingredients should be symbols and the amounts should be strings. Remember that symbols start with a colon and be sure to sub underscores for spaces.
 
-perfect_10_recipe = {:almond_flour => "2 cups",:gluten_free_whole_oats => 
+perfect_10_recipe = {:almond_flour => "2 cups",:gluten_free_whole_oats =>  "1 cup",
+         :kosher_salt => "1/2 tea spoon",
+         :baking_powder => "1/2 tea spoon",
+         :baking_soda => "1/4 tea spoon",
+         :xanthan_Gum => "1/2 tea spoon",
+         :slivered_almonds => "1/4 cup",
+         :mini_dark_chocolate_chips => "3/4 cup",
+         :olive_oil => "1/2 cup"}
 
 
 
