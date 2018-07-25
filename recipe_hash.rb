f1d24346be9dd93e2d8039d05ef38fb95a3c1670
Teresa Rakowsky <teresarakowsky@gmail.com>
2018-07-25 20:16:38 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -25,6 +25,7 @@ perfect_10_recipe = {:almond_flour => "2 cups",:gluten_free_whole_oats =>  "1 cu
 
 
 def recipe_ingredients(perfect_10_recipe)
+  perfect_10_recipe
 end
 
 #2: Write code that returns the amount of chocolate chips from perfect_10 ingredients that is needed in the recipe.
